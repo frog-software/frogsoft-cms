1f0bf95401571674816a1eb76d020fc3203bdf84
NEWBEESL <924989973@qq.com>
2021-09-03 14:24:32 +0800

    refactor(backend): overload UserNotFoundException

--- backend/src/main/java/com/frogsoft/frogsoftcms/exception/user/UserNotFoundException.java
+++ backend/src/main/java/com/frogsoft/frogsoftcms/exception/user/UserNotFoundException.java
@@ -7,4 +7,8 @@ public class UserNotFoundException extends NotFoundException {
   public UserNotFoundException(String username) {
     super("用户名 " + username + " 不存在");
   }
+
+  public UserNotFoundException(Long id) {
+    super("用户名 " + id + " 不存在");
+  }
 }
