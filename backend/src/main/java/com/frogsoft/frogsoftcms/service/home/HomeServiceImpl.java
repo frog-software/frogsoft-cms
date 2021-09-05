b21871bc38ab8e30ef19afff541d802447303c97
Nick-bit233 <58961585+Nick-bit233@users.noreply.github.com>
2021-09-05 21:43:12 +0800

    feat(backend):  add HM02 - Daily Article

--- backend/src/main/java/com/frogsoft/frogsoftcms/service/home/HomeServiceImpl.java
+++ backend/src/main/java/com/frogsoft/frogsoftcms/service/home/HomeServiceImpl.java
@@ -3,6 +3,7 @@ package com.frogsoft.frogsoftcms.service.home;
 import com.frogsoft.frogsoftcms.dto.assembler.article.ArticleModelAssembler;
 import com.frogsoft.frogsoftcms.dto.mapper.article.ArticleMapper;
 import com.frogsoft.frogsoftcms.dto.model.article.ArticleDto;
+import com.frogsoft.frogsoftcms.exception.basic.notfound.NotFoundException;
 import com.frogsoft.frogsoftcms.model.user.User;
 import com.frogsoft.frogsoftcms.repository.article.ArticleRepository;
 import com.frogsoft.frogsoftcms.repository.user.UserRepository;
@@ -12,6 +13,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -30,5 +32,19 @@ public class HomeServiceImpl implements HomeService {
     ).collect(Collectors.toList());
     // TODO：筛选和获取推荐的文章Dto对象
     return articleModelAssembler.toCollectionModel(articles);
+  }
+
+  @Override
+  public EntityModel<ArticleDto> getDailyArticle(){
+    Random randArt = new Random();
+    List<ArticleDto> articles = articleRepository.findAll().stream().map(
+            articleMapper::toArticleDto
+    ).collect(Collectors.toList());
+
+    if(articles.size() <= 0){
+      throw new NotFoundException("目前暂无文章");
+    }
+    // 获取一个随机文章作为每日推荐
+    return articleModelAssembler.toModel(articles.get(randArt.nextInt(articles.size())));
   }
 }
