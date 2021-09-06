cb2a8f6b67996458679a44ba6c8e4aa839e88bac
NEWBEESL <924989973@qq.com>
2021-09-06 11:30:40 +0800

    feat(backend): add sendCode function

--- backend/src/main/java/com/frogsoft/frogsoftcms/service/mail/MailService.java
+++ backend/src/main/java/com/frogsoft/frogsoftcms/service/mail/MailService.java
@@ -1,15 +1,11 @@
 package com.frogsoft.frogsoftcms.service.mail;
 
 
+import com.frogsoft.frogsoftcms.dto.model.user.UserDto;
+import org.springframework.hateoas.EntityModel;
+
 public interface MailService {
 
-  /**
-   * 发送邮件
-   *
-   * @param to         邮件收件人
-   * @param subject    邮件主题
-   * @param verifyCode 邮件验证码
-   */
-  public void sendMail(String to, String subject, String verifyCode);
+  EntityModel<UserDto> sendCode(String username);
 
 }
