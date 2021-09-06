1439133123306687c57570607f16373501a66ea0
sheeplin <1270610465@qq.com>
2021-09-06 19:10:33 +0800

    chore(frontend-user): delete unused dist files

--- frontend-user/dist/assets/About.24f94d92.js
+++
@@ -1,1 +0,0 @@
-import{c as a,o as s,d as e}from"./vendor.678eb57e.js";const o={},n={class:"about"},r=[e("h1",null,"This is an about page",-1)];o.render=function(e,o){return s(),a("div",n,r)};export{o as default};
