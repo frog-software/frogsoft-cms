afcc5765ca914db83621565b8ade5b244dc0761f
Charlie Chiang <charlie_c_0129@outlook.com>
2021-09-08 22:12:15 +0800

    fix(backend): fix file uploading errors

--- backend/src/main/java/com/frogsoft/frogsoftcms/config/FrogsoftCmsConfig.java
+++ backend/src/main/java/com/frogsoft/frogsoftcms/config/FrogsoftCmsConfig.java
@@ -1,8 +1,11 @@
 package com.frogsoft.frogsoftcms.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FrogsoftCmsConfig {
+public class FrogsoftCmsConfig implements WebMvcConfigurer {
 
 }
--- backend/src/main/java/com/frogsoft/frogsoftcms/service/file/FileService.java
+++ backend/src/main/java/com/frogsoft/frogsoftcms/service/file/FileService.java
@@ -2,12 +2,13 @@ package com.frogsoft.frogsoftcms.service.file;
 
 import com.frogsoft.frogsoftcms.dto.model.file.FileDto;
 import com.frogsoft.frogsoftcms.model.user.User;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-  EntityModel<FileDto> putFile(User authenticatedUser, MultipartFile file);
+  EntityModel<FileDto> putFile(User authenticatedUser, MultipartFile file, HttpServletRequest request);
 
   void deleteFile(User authenticatedUser, Long id);
 }
